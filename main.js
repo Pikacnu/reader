3a6e23d7502cdfb5b1352636f64dd31bb4826d87
Pikacnu <92923578+Pikacnu@users.noreply.github.com>
2024-03-05 23:38:19 +0800

    Fix Horizontal Bug and add a button on the screen

--- main.js
+++ main.js
@@ -1,67 +1,49 @@
 const text = document.getElementById('text');
-const totalpage = document.getElementById('total');
-const current = document.getElementById('current');
 const zhheight = document.getElementById('zh-test').offsetHeight / 2;
 const zhwidth = document.getElementById('zh-test').offsetWidth / 2;
 const enheight = document.getElementById('en-test').offsetHeight / 2;
 const enwidth = document.getElementById('en-test').offsetWidth / 2;
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
 const devicewidth = window.innerWidth;
 const deviceheight = window.innerHeight;
 const pageselecter = document.getElementById('pageselecter');
 
 let texts = ``;
-if (!texts) texts = `測試文字，測試文字`;
-
+if (texts === '') texts = `你好，世界！`; // default ch
 let haveChapter = false;
 
 function getpages(isHorizontal, textinput) {
 	if (isHorizontal) {
 		const maxheight = Math.round(text.offsetHeight / zhheight) / 2;
 		const maxwidth = Math.round(text.offsetWidth / zhwidth) - 1;
-		let temp = textinput;
-		let data = temp
+		console.log(maxheight, maxwidth);
+		let data = textinput
 			.replaceAll('\n', '₩')
 			.replaceAll(/，/g, '，₩')
 			.replaceAll(/。/g, '。₩')
 			.replaceAll('　', '')
+			.replaceAll(/ /g, '')
 			.split('₩')
 			.map((e) => {
 				if (e.length >= maxwidth) {
-					return e.match(new RegExp(`.{1,${maxwidth - 1}}`, 'g')).flat(1);
+					let test = e.match(new RegExp(`.{1,${maxwidth - 1}}`, 'g'));
+					let result = [];
+					for (let i = 0; i < test.length; i += 1) {
+						result.push(test[i]);
+					}
+					return result;
 				}
 				return e;
-			})
-			.reduce((acc, cur) => {
-				if (!Array.isArray(acc)) acc = [acc];
-				if (acc.length > 1) {
-					const last = acc.pop();
-					if (acc[acc.length - 1].includes(last)) {
-						return acc.concat(cur);
-					}
-					if (last.length + cur.length <= maxwidth) {
-						acc.push(last + cur);
-					} else {
-						acc.push(last);
-						acc.push(cur);
-					}
-				} else {
-					acc.push(cur);
-				}
-				return acc;
 			})
 			.map((e) => `<span>${e}</span>`)
 			.reduce((acc, cur) => {
 				if (!Array.isArray(acc[acc.length - 1])) return [[acc, cur]];
-				if (acc[acc.length - 1].length < maxheight / 3) {
+				if (acc[acc.length - 1].length < maxwidth) {
 					acc[acc.length - 1].push(cur);
 					return acc;
 				}
 				acc.push([cur]);
 				return acc;
 			});
-		console.log(maxheight, maxwidth);
 		return data;
 	}
 
@@ -117,8 +99,8 @@ function getpages(isHorizontal, textinput) {
 			return acc;
 		}, [])
 		.map((e) => {
-			if (e.match(/[a-zA-Z0-9]/g)) {
-				return e.replaceAll(/([a-zA-Z0-9]+)/g, '<span>$1</span>');
+			if (e.match(/([^\u4E00-\u9FFF\n]+)/gm)) {
+				return e.replaceAll(/([^\u4E00-\u9FFF\n]+)/gm, '<span>$1</span>');
 			}
 			return e;
 		})
@@ -141,6 +123,7 @@ function pageupdate(page) {
 }
 
 //chapter
+// Change Chapter
 
 const chapterprev = document.getElementById('chap-prev');
 const chapternext = document.getElementById('chap-next');
@@ -166,13 +149,9 @@ if (haveChapter) {
 	chapterprev.style.display = 'none';
 }
 function getChapter(text) {
-	text = text
-		.split(
-			/------------------------------ 第\d章 ------------------------------/gm,
-		)
-		.map((e) => {
-			return getpages(false, e);
-		});
+	text = text.split(/-+ 第\d章 -+/gm).map((e) => {
+		return getpages(false, e);
+	});
 	text.shift();
 	return text;
 }
@@ -181,13 +160,17 @@ let chapters = getChapter(texts);
 let totalchapters = chapters.length;
 
 function chapterupdate(num) {
+	if (!(chapter > 1 && chapter < totalchapters)) return;
 	pages = chapters[num];
 	pageupdate(1);
 	page = 1;
-	document.title = `第${num + 1}章`;
+	document.title = `第${num}章`;
 }
 
-//init
+// Init for first page
+
+const totalpage = document.getElementById('total');
+const current = document.getElementById('current');
 
 let pages = getpages(false, texts);
 let totalpages = pages.length;
@@ -196,14 +179,22 @@ total.innerHTML = pages.length;
 text.innerHTML = pages[0].join('');
 current.innerHTML = 1;
 
-prev.addEventListener('click', prevpage);
-next.addEventListener('click', nextpage);
+const prev = document.getElementById('prev');
+const touchprev = document.getElementById('touchprev');
+const next = document.getElementById('next');
+const touchnext = document.getElementById('touchnext');
+
+prev.addEventListener('click', nextpage);
+next.addEventListener('click', prevpage);
+touchnext.addEventListener('click', nextpage);
+touchprev.addEventListener('click', prevpage);
 
 function nextpage() {
 	if (page < totalpages) {
 		prev.disable = false;
 		page++;
 		pageupdate(page);
+		if (haveChapter && page === totalpages) chapterupdate(chapter + 1);
 	} else {
 		next.disable = true;
 	}
@@ -214,6 +205,7 @@ function prevpage() {
 		next.disable = false;
 		page--;
 		pageupdate(page);
+		if (haveChapter && page === 1) chapterupdate(chapter - 1);
 	} else {
 		prev.disable = true;
 	}
@@ -223,9 +215,8 @@ document.body.addEventListener('keydown', (_) => {
 	if (_.key === 'ArrowLeft') prevpage();
 });
 
-//page ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-const settingbar = document.getElementById('settingbar');
+//Day/Night mode
+// Change Light Mode and Dark Mode
 
 let light = false;
 
@@ -243,7 +234,9 @@ const lightupdate = (_) => {
 };
 
 lightchanger.addEventListener('click', lightupdate);
-//font setting
+
+// Font setting tab
+// Change Font Setting Open
 
 const fontsetting = document.getElementById('fontsettingtab');
 let fontsettingopen = false;
@@ -256,7 +249,8 @@ fontsetting.addEventListener('click', (_) => {
 	settingclicked = true;
 });
 
-//font resizer
+// Font Size
+// Change Font Size
 
 const fontresizer = document.getElementById('fontsize');
 
@@ -273,7 +267,8 @@ fontresizer.addEventListener('input', (_) => {
 	);
 });
 
-//font bold
+//Font bold
+// Change Font Weight
 
 const fontbold = document.getElementById('fontbold');
 
@@ -281,7 +276,8 @@ fontbold.addEventListener('input', (_) => {
 	document.body.style.setProperty('--font-weight', _.target.value);
 });
 
-//color setting tab
+// Color setting tab
+// Change Color Setting Open
 
 const colorsetting = document.getElementById('colorsettingtab');
 let colorsettingopen = false;
@@ -294,7 +290,8 @@ colorsetting.addEventListener('click', (_) => {
 	settingclicked = true;
 });
 
-//textcolor setting
+//Textcolor setting
+// Change Text Color
 
 const textcolor = document.getElementById('textcolor');
 textcolor.value = document.body.style.getPropertyValue('--text-color');
@@ -309,7 +306,8 @@ textcolor.addEventListener('click', (e) => {
 	settingclicked = true;
 });
 
-//background color setting
+//Background color setting
+// Change Background Color
 
 const backgroundcolor = document.getElementById('backgroundcolor');
 backgroundcolor.value = document.body.style.getPropertyValue('--text-color');
@@ -332,9 +330,8 @@ backgroundcolor.addEventListener('click', (e) => {
 	settingclicked = true;
 });
 
-//setting bar
-
-//search text in page
+//Setting Bar
+// high light search result and remove search result when click on it
 
 const searchtab = document.getElementById('searchtab');
 
@@ -398,7 +395,8 @@ function removeHighLight() {
 	text.innerHTML = pages[page - 1].join('');
 }
 
-//text direction
+//Text Direction
+// Change Text Direction
 
 let isHorizontal = false;
 
@@ -410,18 +408,21 @@ textdirection.addEventListener('click', (_) => {
 		? '<svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m14.6961816 11.6470802.0841184.0726198 2 2c.2662727.2662727.2904793.682876.0726198.9764816l-.0726198.0841184-2 2c-.2929.2929-.7677.2929-1.0606 0-.2662727-.2662727-.2904793-.682876-.0726198-.9764816l.0726198-.0841184.7196-.7197h-10.6893c-.41421 0-.75-.3358-.75-.75 0-.3796833.28215688-.6934889.64823019-.7431531l.10176981-.0068469h10.6893l-.7196-.7197c-.2929-.2929-.2929-.7677 0-1.0606.2662727-.2662727.682876-.2904793.9764816-.0726198zm-8.1961616-8.6470802c.30667 0 .58246.18671.69635.47146l3.00003 7.50004c.1538.3845-.0333.821-.41784.9749-.38459.1538-.82107-.0333-.9749-.4179l-.81142-2.0285h-2.98445l-.81142 2.0285c-.15383.3846-.59031.5717-.9749.4179-.38458-.1539-.57165-.5904-.41781-.9749l3-7.50004c.1139-.28475.38968-.47146.69636-.47146zm8.1961616 1.14705264.0841184.07261736 2 2c.2662727.26626364.2904793.68293223.0726198.97654222l-.0726198.08411778-2 2c-.2929.29289-.7677.29289-1.0606 0-.2662727-.26626364-.2904793-.68293223-.0726198-.97654222l.0726198-.08411778.7196-.7196675h-3.6893c-.4142 0-.75-.3357925-.75-.7500025 0-.3796925.2821653-.69348832.6482323-.74315087l.1017677-.00684663h3.6893l-.7196-.7196725c-.2929-.29289-.2929-.76777 0-1.06066.2662727-.26626364.682876-.29046942.9764816-.07261736zm-8.1961616 1.62238736-.89223 2.23056h1.78445z" fill="#212121"/></svg>'
 		: '<svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m13.75 12c.4142 0 .75.3358.75.75v1.6894l.7197-.7197c.2929-.2929.7677-.2929 1.0606 0s.2929.7677 0 1.0606l-2 2c-.0753.0754-.1627.1313-.2559.1679-.0489333.0192667-.1003556.0335111-.1536444.0421407l-.1207556.0097093-.0395-.00105c-.0817-.0043-.16-.0216-.2327-.0499-.0941-.0366-.1822-.0928-.2581-.1688l-2-2c-.2929-.2929-.2929-.7774 0-1.0703s.7677-.2832 1.0606.0097l.7197.7197v-1.6894c0-.4142.3358-.75.75-.75zm-8-9c.41421 0 .7500025.33579.7500025.75v10.6893l.7196675-.7196c.29289-.2929.76777-.2929 1.06066 0s.29289.7677 0 1.0606l-2 2c-.29289.2929-.76777.2929-1.06066 0l-2-2c-.29289-.2929-.29289-.7677 0-1.0606s.76777-.2929 1.06066 0l.7196725.7196v-10.6893c0-.41421.3357875-.75.7499975-.75zm7.75 0c.3067 0 .5825.18671.6964.47146l3 7.50004c.1538.3845-.0333.821-.4178.9749-.3846.1538-.8211-.0333-.9749-.4179l-.8115-2.0285h-2.9844l-.8114 2.0285c-.1539.3846-.5903.5717-.9749.4179-.38461-.1539-.57168-.5904-.41784-.9749l3.00004-7.50004c.1139-.28475.3896-.47146.6963-.47146zm0 2.76944-.8922 2.23056h1.7844z" fill="#212121"/></svg>';
 	text.classList.toggle('column');
-	pages = getpages(isHorizontal);
+	pages = getpages(isHorizontal, texts);
 	pageupdate(page);
 	settingclicked = true;
 });
 
 addEventListener('resize', () => {
-	pages = getpages(isHorizontal);
+	pages = getpages(isHorizontal, texts);
 	page = 1;
 	pageupdate(page);
 });
 
 //setting bar
+
+const settingbar = document.getElementById('settingbar');
+const touchsetting = document.getElementById('touchsetting');
 
 const toggleSettingBar = () => {
 	if (settingclicked) return (settingclicked = false);
@@ -432,9 +433,14 @@ const toggleSettingBar = () => {
 };
 
 document.body.addEventListener('click', (e) => {
-	if (!settingbar.contains(e.target) && isSettingbarOpen) {
+	if (
+		!settingbar.contains(e.target) &&
+		!touchsetting.contains(e.target) &&
+		isSettingbarOpen
+	) {
 		toggleSettingBar();
 	}
 });
 
 settingbar.addEventListener('click', toggleSettingBar);
+touchsetting.addEventListener('click', toggleSettingBar);

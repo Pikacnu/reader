6fa483b744ec711bf46230ab0a26e423cbab8369
Pikacnu <92923578+Pikacnu@users.noreply.github.com>
2024-05-30 00:16:50 +0800

    remove Services folder

--- app/services/db.server.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const migrationsClient = postgres(process.env.SQL_CONNECTION_LINK||'', {
-	max: 1,
-});
-export const db = drizzle(migrationsClient);
